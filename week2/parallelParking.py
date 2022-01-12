f6c5be6addfbaa865ff8e928edba0c3e8a7a3148
shubham-p-patil <shubhamppatil30@gmail.com>
2022-01-11 23:11:36 -0800

    update parallel sample test

--- week2/parallelParking.py
+++ week2/parallelParking.py
@@ -9,30 +9,48 @@ import time
 if __name__ == "__main__":
     try:
         px = Picarx()
+
         px.forward(30)
+        time.sleep(0.5)
         
-        time.sleep(3)
+        px.forward(0)
+        time.sleep(1)
 
         for angle in range(0,-35,-1):
             px.set_dir_servo_angle(angle)
-            time.sleep(0.1)   
+            time.sleep(0.01)   
         
+        px.forward(0)
+        time.sleep(1)
+
         px.backward(30)
-        time.sleep(3)
+        time.sleep(2)
         
+        px.forward(0)
+        time.sleep(1)
+
         for angle in range(-35,35):
             px.set_dir_servo_angle(angle)
-            time.sleep(0.1)   
+            time.sleep(0.01)   
     
+        px.forward(0)
+        time.sleep(1)
+        
         px.forward(20)
         time.sleep(1)   
 
+        px.forward(0)
+        time.sleep(1)
+        
         for angle in range(35,0,-1):
             px.set_dir_servo_angle(angle)
-            time.sleep(0.1)
+            time.sleep(0.01)
+        
+        px.forward(0)
+        time.sleep(1)
         
         px.forward(10)
-        time.sleep(2)   
+        time.sleep(0.2)   
 
         px.forward(0)
         time.sleep(1)
