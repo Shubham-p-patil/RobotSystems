783aa6d732b8dcda2adb9f00023fffa736447c82
shubham-p-patil <shubhamppatil30@gmail.com>
2022-01-11 22:15:04 -0800

    add sample forward test

---
+++ week2/forwardBackward.py
@@ -0,0 +1,20 @@
+import sys
+sys.path.append(r'/home/pi/RobotSystems/lib')
+from utils import reset_mcu
+reset_mcu()
+
+from picarx import Picarx
+import time
+
+if __name__ == "__main__":
+    try:
+        px = Picarx()
+        px.forward(30)
+        time.sleep(0.5)
+        px.backward(30)
+        time.sleep(0.5)
+
+        px.forward(0)
+        time.sleep(1)
+    finally:
+        px.forward(0)
