d1b5550208bf8394dba716ba937aae19b9fac566
shubham-p-patil <shubhamppatil30@gmail.com>
2022-01-18 00:31:32 -0800

    update picarx_improved

--- lib/picarx_improved.py
+++ lib/picarx_improved.py
@@ -185,50 +185,42 @@ class Picarx(object):
             pass
 
     def backward(self,speed):
-        try:
-            current_angle = self.dir_current_angle
-            if current_angle != 0:
-                abs_current_angle = abs(current_angle)
-                # if abs_current_angle >= 0:
-                if abs_current_angle > 40:
-                    abs_current_angle = 40
-                power_scale = (100 - abs_current_angle) / 100.0 
-                print("power_scale:",power_scale)
-                if (current_angle / abs_current_angle) > 0:
-                    self.set_motor_speed(1, -1*speed)
-                    self.set_motor_speed(2, speed * power_scale)
-                else:
-                    self.set_motor_speed(1, -1*speed * power_scale)
-                    self.set_motor_speed(2, speed )
-            else:
+        current_angle = self.dir_current_angle
+        if current_angle != 0:
+            abs_current_angle = abs(current_angle)
+            # if abs_current_angle >= 0:
+            if abs_current_angle > 40:
+                abs_current_angle = 40
+            power_scale = (100 - abs_current_angle) / 100.0 
+            print("power_scale:",power_scale)
+            if (current_angle / abs_current_angle) > 0:
                 self.set_motor_speed(1, -1*speed)
-                self.set_motor_speed(2, speed) 
-        except:
-            print("Moving backward with speed:{0}".format(speed))
-            pass 
+                self.set_motor_speed(2, speed * power_scale)
+            else:
+                self.set_motor_speed(1, -1*speed * power_scale)
+                self.set_motor_speed(2, speed )
+        else:
+            self.set_motor_speed(1, -1*speed)
+            self.set_motor_speed(2, speed) 
 
     def forward(self,speed):
-        try:
-            current_angle = self.dir_current_angle
-            if current_angle != 0:
-                abs_current_angle = abs(current_angle)
-                # if abs_current_angle >= 0:
-                if abs_current_angle > 40:
-                    abs_current_angle = 40
-                power_scale = (100 - abs_current_angle) / 100.0 
-                print("power_scale:",power_scale)
-                if (current_angle / abs_current_angle) > 0:
-                    self.set_motor_speed(1, speed)
-                    self.set_motor_speed(2, -1*speed * power_scale)
-                else:
-                    self.set_motor_speed(1, speed * power_scale)
-                    self.set_motor_speed(2, -1*speed )
-            else:
+        current_angle = self.dir_current_angle
+        if current_angle != 0:
+            abs_current_angle = abs(current_angle)
+            # if abs_current_angle >= 0:
+            if abs_current_angle > 40:
+                abs_current_angle = 40
+            power_scale = (100 - abs_current_angle) / 100.0 
+            print("power_scale:",power_scale)
+            if (current_angle / abs_current_angle) > 0:
                 self.set_motor_speed(1, speed)
-                self.set_motor_speed(2, -1*speed)
-        except:
-            print("Moving forward with speed:{0}".format(speed))
-            pass                  
+                self.set_motor_speed(2, -1*speed * power_scale)
+            else:
+                self.set_motor_speed(1, speed * power_scale)
+                self.set_motor_speed(2, -1*speed )
+        else:
+            self.set_motor_speed(1, speed)
+            self.set_motor_speed(2, -1*speed)
 
     def stop(self):
         try:
